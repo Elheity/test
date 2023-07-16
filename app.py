bc832efa16a9efa7077d8a6531571060dbe32060
Elheity <104684141+Elheity@users.noreply.github.com>
2023-07-16 08:24:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,98 @@ from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.model_selection import cross_val_score, train_test_split
 import numpy as np 
 import hashlib
+from Bio.SeqUtils.ProtParam import ProteinAnalysis
 
 
+
+# PhysioChemical Properties of Amino acids
+
+#Aromaticity
+def calculate_aromaticity(row):
+  sequence = str(row[1])
+  X = ProteinAnalysis(sequence)
+  return "%0.2f" % X.aromaticity()
+
+#Molecular Weight
+def calculate_molecular_weight(row):
+  sequence = str(row[1])
+  X = ProteinAnalysis(sequence)
+  return "%0.2f" % X.molecular_weight()
+
+#Instability Index
+def calculate_instability_index(row):
+  sequence = str(row[1])
+  X = ProteinAnalysis(sequence)
+  return "%0.2f" % X.instability_index()
+
+#Hydrophobicity
+def calculate_hydrophobicity(row):
+  sequence = str(row[1])
+  X = ProteinAnalysis(sequence)
+  return "%0.2f" % X.gravy(scale='KyteDoolitle')
+
+#Isoelectric Point
+def calculate_isoelectric_point(row):
+  sequence = str(row[1])
+  X = ProteinAnalysis(sequence)
+  return "%0.2f" % X.isoelectric_point()
+
+#Charge
+def calculate_charge(row):
+  sequence = str(row[1])
+  X = ProteinAnalysis(sequence)
+  return "%0.2f" % X.charge_at_pH(row[2])
+# Define the hashing function
 # Define the hashing function
 def hash_func(text):
     return int(hashlib.sha256(text.encode('utf-8')).hexdigest(), 16) % 10**8
 
-train_data = pd.read_csv("data/train_data.csv")
+
 training = train_data
-training["protein_sequence"] = training["protein_sequence"].apply(hash_func)
-training["data_source"] = training["data_source"].apply(hash_func)
-features = training.drop(["seq_id","tm"], axis = 1)
+training=training.dropna(how='all')
+training['pH'] = training['pH'].fillna(training['pH'].mean())
+training.drop_duplicates(subset=['protein_sequence','pH','data_source'],inplace=True)
+training = training.drop(['data_source'],axis=1)
+
+amino_count = training['protein_sequence'].str.split('').explode().value_counts().drop('')
+
+# Protein Sequence Length as a column
+training["protein_length"] = training["protein_sequence"].apply(lambda x: len(x))
+
+def return_amino_acid_df(df):
+  # Feature Engineering on Train Data
+  amino_acids=['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+  for amino_acid in amino_acids:
+    df[amino_acid]=df['protein_sequence'].str.count(amino_acid,re.I)/df['protein_length']
+    #df[amino_acid]=df['protein_sequence'].str.count(amino_acid,re.I)
+  return df
+
+training = return_amino_acid_df(training)
+
+#training["protein_sequence"] = training["protein_sequence"].apply(hash_func)
+#training["data_source"] = training["data_source"].apply(hash_func)
+training['Aromaticity'] = training.apply(calculate_aromaticity, axis=1)
+training['Molecular Weight'] = training.apply(calculate_molecular_weight, axis=1)
+training['Instability Index'] = training.apply(calculate_instability_index, axis=1)
+training['Hydrophobicity'] = training.apply(calculate_hydrophobicity, axis=1)
+training['Isoelectric Point'] = training.apply(calculate_isoelectric_point, axis=1)
+training['Charge'] = training.apply(calculate_charge, axis=1)
+
+training.drop(columns=["protein_length"], inplace=True)
+training.drop(columns=["protein_sequence", "seq_id"], inplace=True)
+
+# Reset the DataFrame indexes
+training.reset_index(drop=True, inplace=True)
+
+
+training['Aromaticity'] = pd.to_numeric(training['Aromaticity'])
+training['Molecular Weight'] = pd.to_numeric(training['Molecular Weight'])
+training['Instability Index'] = pd.to_numeric(training['Instability Index'])
+training['Hydrophobicity'] = pd.to_numeric(training['Hydrophobicity'])
+training['Isoelectric Point'] = pd.to_numeric(training['Isoelectric Point'])
+training['Charge'] = pd.to_numeric(training['Charge'])
+
+features = training.drop(["tm"], axis = 1)
 target = training["tm"]
 #x = np.array(data['YearsExperience']).reshape(-1,1)
 #lr = LinearRegression()
@@ -61,8 +142,48 @@ if nav == "Prediction":
         'pH' : val2,
         'data_source' : val3,
         }, index=['row1'])
-    test_data["protein_sequence"] = test_data["protein_sequence"].apply(hash_func)
-    test_data["data_source"] = test_data["data_source"].apply(hash_func)
+    
+    test_data = test_data.drop(['data_source'],axis=1)
+
+    amino_count = test_data['protein_sequence'].str.split('').explode().value_counts().drop('')
+
+    # Protein Sequence Length as a column
+    test_data["protein_length"] = test_data["protein_sequence"].apply(lambda x: len(x))
+
+          
+
+    test_data = return_amino_acid_df(test_data)
+          #print("shape", test_data.shape)
+          #test_data["protein_sequence"] = test_data["protein_sequence"].apply(hash_func)
+          #test_data["data_source"] = test_data["data_source"].apply(hash_func)
+    test_data['Aromaticity'] = test_data.apply(calculate_aromaticity, axis=1)
+    test_data['Molecular Weight'] = test_data.apply(calculate_molecular_weight, axis=1)
+    test_data['Instability Index'] = test_data.apply(calculate_instability_index, axis=1)
+    test_data['Hydrophobicity'] = test_data.apply(calculate_hydrophobicity, axis=1)
+    test_data['Isoelectric Point'] = test_data.apply(calculate_isoelectric_point, axis=1)
+    test_data['Charge'] = test_data.apply(calculate_charge, axis=1)
+
+    test_data.drop(columns=["protein_length"], inplace=True)
+    test_data.drop(columns=["protein_sequence"], inplace=True)
+
+          # Reset the DataFrame indexes
+    test_data.reset_index(drop=True, inplace=True)
+
+
+    test_data['Aromaticity'] = pd.to_numeric(test_data['Aromaticity'])
+    test_data['Molecular Weight'] = pd.to_numeric(test_data['Molecular Weight'])
+    test_data['Instability Index'] = pd.to_numeric(test_data['Instability Index'])
+    test_data['Hydrophobicity'] = pd.to_numeric(test_data['Hydrophobicity'])
+    test_data['Isoelectric Point'] = pd.to_numeric(test_data['Isoelectric Point'])
+    test_data['Charge'] = pd.to_numeric(test_data['Charge'])
+
+          #Bestfeatures= 
+          #test_data["protein_sequence"] = test_data["protein_sequence"].apply(hash_func)
+          #test_data["data_source"] = test_data["data_source"].apply(hash_func)
+          #val = np.array(val).reshape(1,-1)
+    if st.checkbox("Show Table"):
+        st.table(test_data.head(20))
+
     #val = np.array(val).reshape(1,-1)
     pred =gb_model.predict(test_data)
 
